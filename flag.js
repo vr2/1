88c1170d93f42c0976d5fe34863ac5a55c785bb3
vr2 <hydw9898@gmail.com>
2024-11-05 09:46:56 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,3 +1,15 @@
-alasql.fn.add = function(a, b) {
-    return a + b; // 두 수를 더하는 함수
-};
+// /flag 경로에서 데이터 가져오기
+fetch('/flag')
+    .then(response => {
+        // 응답이 성공적이지 않은 경우 오류 처리
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); // JSON으로 응답 변환
+    })
+    .then(data => {
+        console.log(data); // 가져온 데이터 출력
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
