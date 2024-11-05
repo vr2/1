e4ca9630bb06b7e74ca42d9874b9b51e7409c03c
vr2 <hydw9898@gmail.com>
2024-11-05 13:14:07 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const alasql = require('alasql');
 
-// getFlagContent 함수를 alasql.fn에 등록
-alasql.fn.getFlagContent = function() {
+// getFlagContent 함수를 fs 모듈을 통해 직접 정의
+function getFlagContent() {
     try {
         const data = fs.readFileSync('/flag', 'utf-8');
         return data;  // 파일 내용을 반환
@@ -10,8 +10,11 @@ alasql.fn.getFlagContent = function() {
         console.error("Error reading the /flag file:", err);
         return null;  // 오류 발생 시 null 반환
     }
-};
+}
 
-// alasql을 사용하여 함수 호출 예제
+// getFlagContent를 alasql.fn에 등록
+alasql.fn.getFlagContent = getFlagContent;
+
+// alasql에서 함수 호출 테스트
 const result = alasql('SELECT VALUE getFlagContent()');
 console.log("Flag Content:", result);
