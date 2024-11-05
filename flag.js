42876273c3ca9aa0cb68e32ec088e9df4f0689a3
vr2 <hydw9898@gmail.com>
2024-11-05 09:50:44 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,15 +1,4 @@
-// /flag 경로에서 데이터 가져오기
-fetch('/flag')
-    .then(response => {
-        // 응답이 성공적이지 않은 경우 오류 처리
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json(); // JSON으로 응답 변환
-    })
-    .then(data => {
-        console.log(data); // 가져온 데이터 출력
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+// flag.js
+alasql.fn.getFlag = function() {
+    return "This is a flag string."; // 예시로 문자열 반환
+};
