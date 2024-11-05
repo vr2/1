8ec52852493bea35ddba128a707a3234300bf04b
vr2 <hydw9898@gmail.com>
2024-11-05 11:05:50 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 // /flag 파일을 실행하고 플래그를 가져오는 함수 정의
-function runFlagFile() {
+alasql.fn.getFlag = function () {
     exec('/flag', (error, stdout, stderr) => {
         return stdout.trim
     });
