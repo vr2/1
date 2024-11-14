ed903d9256cc450b2045baaf81608bbc66b4e4ab
vr2 <hydw9898@gmail.com>
2024-11-14 10:46:29 +0900

    Update flag2.js

--- flag2.js
+++ flag2.js
@@ -1,9 +1,11 @@
-alasql("CREATE TABLE example1 (a INT, b STRING)");
+const alasql = require('alasql');
+alasql("CREATE TABLE example1 (a INT, b INT)");
 
 // alasql's data store for a table can be assigned directly
 alasql.tables.example1.data = [
-    {a:2,b:"TEXT"}
+    {a:1,b:2},
+    {a:3,b:4}
 ];
 
 // ... or manipulated with normal SQL
-alasql("INSERT INTO example1 VALUES (1,'TEXT')");
+alasql("INSERT INTO example1 VALUES (1,5)");
