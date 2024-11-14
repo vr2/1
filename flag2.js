19318b3551fc5dd5196bc6c630240a4d5bdde82f
vr2 <hydw9898@gmail.com>
2024-11-14 10:19:54 +0900

    Update flag2.js

--- flag2.js
+++ flag2.js
@@ -1,3 +1,2 @@
-alasql("CREATE TABLE example1 (a STRING)");
-alasql("INSERT INTO example1 VALUES ('text')");
-
+alasql("CREATE TABLE example1 (a INT)");
+alasql("INSERT INTO example1 VALUES (1337)");
