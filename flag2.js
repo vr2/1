ee1a40dd467ffa212cce313f2c208000e377cd6f
vr2 <hydw9898@gmail.com>
2024-11-14 10:44:08 +0900

    Update flag2.js

--- flag2.js
+++ flag2.js
@@ -1,2 +1,10 @@
-alasql("CREATE TABLE woo (a INT)");
-alasql("INSERT INTO woo VALUES (1337)");
+alasql("CREATE TABLE example1 (a INT, b INT)");
+
+// alasql's data store for a table can be assigned directly
+alasql.tables.example1.data = [
+    {a:2,b:6},
+    {a:3,b:4}
+];
+
+// ... or manipulated with normal SQL
+alasql("INSERT INTO example1 VALUES (1,5)");
