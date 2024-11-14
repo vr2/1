dbda6804c732a52af86b95bf4b58a20ac2035f47
vr2 <hydw9898@gmail.com>
2024-11-14 10:45:48 +0900

    Update flag2.js

--- flag2.js
+++ flag2.js
@@ -2,7 +2,7 @@ alasql("CREATE TABLE example1 (a INT, b STRING)");
 
 // alasql's data store for a table can be assigned directly
 alasql.tables.example1.data = [
-    {a:2,b:TEXT}
+    {a:2,b:"TEXT"}
 ];
 
 // ... or manipulated with normal SQL
