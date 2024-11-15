50df18823503e2dfedde95c36a6efad6d3099bac
vr2 <hydw9898@gmail.com>
2024-11-15 09:48:06 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ alasql('CREATE TABLE genie (content TEXT)');
 alasql.tables.genie.data = [
     {content:"test"}
 ];
-alasql('INSERT INTO genie VALUES (executeFile("/FLAG"))');
+alasql('INSERT INTO genie VALUES ("aaaa")');
