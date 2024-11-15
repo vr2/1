f374f5902333d895c4c8fe1112d95c7affc90bf6
vr2 <hydw9898@gmail.com>
2024-11-15 09:33:27 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 alasql("CREATE TABLE example1 (a INT, b INT)");
+alasql.tables.example1.data = [
+    {a:2,b:6},
+    {a:3,b:4}
+];
 
 alasql("CREATE TABLE test2 (content TEXT)");
 alasql("INSERT INTO test2 VALUES ("test")");
