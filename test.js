a6fa46e7b417b1a013cf987c4ff65b79378c5ad4
vr2 <hydw9898@gmail.com>
2024-11-15 09:35:36 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 alasql("CREATE TABLE example1 (a INT, b INT)");
+
+// alasql's data store for a table can be assigned directly
 alasql.tables.example1.data = [
     {a:2,b:6},
     {a:3,b:4}
 ];
+
+// ... or manipulated with normal SQL
 alasql("INSERT INTO example1 VALUES (1,5)");
+
+var res = alasql("SELECT * FROM example1 ORDER BY b DESC");
+
+console.log(res); // [{a:2,b:6},{a:1,b:5},{a:3,b:4}]
